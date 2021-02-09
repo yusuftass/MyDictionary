72ddf37bb788844250301427985048f25f40cc28
yusuf taş <yusuf.yusuf21@hotmail.com>
2021-02-09 20:16:17 +0300

    Add project files.

---
+++ MyDictionary/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyEditor<int, string> dictionary = new MyEditor<int, string>();
+            dictionary.Add(1, "BB");
+            dictionary.Add(3, "FF");
+            dictionary.Add(1, "AA");
+            dictionary.Add(2, "CC");
+            dictionary.Add(2, "DD");
+
+            foreach (var item in dictionary.Dictionary)
+            {
+                Console.Write("Key => " + item.Key + ", ");
+                var count = 1;
+                foreach (var value in item.Value)
+                {
+                    if (count < item.Value.Count)
+                    {
+                        Console.Write(value + ", ");
+                    }
+                    else
+                    {
+                        Console.WriteLine(value);
+                    }
+                    count++;
+                }
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    public class MyEditor<U, T>
+    {
+        Dictionary<U, List<T>> dictionary;
+
+        public MyEditor()
+        {
+            dictionary = new Dictionary<U, List<T>>();
+        }
+
+        public void Add(U key, T value)
+        {
+            List<T> list;
+            if (this.dictionary.TryGetValue(key, out list))
+            {
+                list.Add(value);
+            }
+            else
+            {
+                list = new List<T>();
+                list.Add(value);
+                this.dictionary[key] = list;
+            }
+        }
+
+        public Dictionary<U, List<T>> Dictionary
+        {
+            get { return dictionary; }
+        }
+    }
+
+}
